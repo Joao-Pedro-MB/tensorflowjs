8bd84b217b483a4b8fdc4901fed3e793f51787f7
Joao Pedro De Moraes Bonucci <joao.bonucci@ifood.com.br>
2023-09-22 00:37:40 -0300

    simple linear model

--- modelMetrics/app.js
+++ modelMetrics/app.js
@@ -10,168 +10,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // Simulate battery info on button click
     const simulateButton = document.getElementById("simulateButton");
     simulateButton.addEventListener("click", () => {
-      printBatteryInfo();
+      startModel();
     });
   });
   
 
-// Function to add content to the reports div
-function addToReports(content) {
-  const reportsDiv = document.getElementById("reports");
-  const paragraph = document.createElement("p");
-  paragraph.textContent = content;
-  reportsDiv.appendChild(paragraph);
-}
-  
-async function handleFormSubmit(event) {
-  event.preventDefault();
-  
-  const selectedSensors = [];
-  const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-  
-    checkboxes.forEach((checkbox) => {
-      selectedSensors.push(checkbox.value);
-    });
-  
-  // Start training the TensorFlow.js model here
-  //await startModel(selectedSensors);
-  
-    // Print sensor values based on selected checkboxes
-    if (selectedSensors.includes("battery")) {
-      printBatteryInfo(addToReports);
-    }
-    if (selectedSensors.includes("hardware")) {
-        printHardwareInfo(addToReports);
-    }
-    if (selectedSensors.includes("location")) {
-      startGeolocation(addToReports);
-    }
-    //if (selectedSensors.includes("gyroscope")) {
-    //  startGyroscope(addToReports);
-    //}
-    if (selectedSensors.includes("auxiliarySensors")) {
-      startAccelerometer(addToReports);
-      startAmbientLightSensor(addToReports);
-      startProximitySensor(addToReports);
-    }
-  
-    // Print sensor data every 5 seconds
-    const interval = setInterval(() => {
-      if (selectedSensors.includes("battery")) {
-        printBatteryInfo(addToReports);
-      }
-      if (selectedSensors.includes("hardware")) {
-        printHardwareInfo(addToReports);
-      }
-      if (selectedSensors.includes("location")) {
-        startGeolocation(addToReports);
-      }
-      //if (selectedSensors.includes("gyroscope")) {
-      //  startGyroscope(addToReports);
-      //}
-      if (selectedSensors.includes("auxiliarySensors")) {
-        startAccelerometer(addToReports);
-      }
-    }, 5000);
-  
-    // Stop printing after 5 minutes (300 seconds)
-    setTimeout(() => {
-      clearInterval(interval);
-    }, 300000); // 5 minutes in milliseconds
-  }
-  
-  function printHardwareInfo(addToReports) {
-    const memoryUsage = performance.memory;
-    const processorInfo = navigator.hardwareConcurrency;
-  
-    // Print browser, processor, and memory usage data
-    addToReports(`Memory used: ${memoryUsage.usedJSHeapSize} bytes`);
-    addToReports(`Memory total: ${memoryUsage.totalJSHeapSize} bytes`);
-    addToReports(`Processor cores: ${processorInfo}`);
-  }
-  
-  function printBatteryInfo(addToReports) {
-    navigator.getBattery().then((battery) => {
-      addToReports(`Battery charging? ${battery.charging ? "Yes" : "No"}`);
-      addToReports(`Battery level: ${battery.level * 100}%`);
-      addToReports(`Battery charging time: ${battery.chargingTime} seconds`);
-      addToReports(`Battery discharging time: ${battery.dischargingTime} seconds`);
-    });
-  }
-  
-  function startGeolocation(addToReports) {
-    function success(position) {
-      addToReports(`Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`);
-    }
-  
-    function error() {
-      addToReports("Sorry, no position available.");
-    }
-  
-    const options = {
-      enableHighAccuracy: true,
-      maximumAge: 30000,
-      timeout: 27000,
-    };
-  
-    const watchID = navigator.geolocation.watchPosition(success, error, options);
-  }
-  
-  //function startGyroscope(addToReports) {
-  //  if ('Gyroscope' in window) {
-  //    const gyro = new Gyroscope({ frequency: 60 });
-  //
-  //    gyro.addEventListener('reading', () => {
-  //      addToReports('Gyroscope values:');
-  //      addToReports(`x: ${gyro.x}`);
-  //      addToReports(`y: ${gyro.y}`);
-  //      addToReports(`z: ${gyro.z}`);
-  //    });
-  //
-  //    gyro.start();
-  //  } else {
-  //    addToReports('Warning: Gyroscope not supported on this device.');
-  //  }
-  //}
-  
-  function startProximitySensor(addToReports) {
-    if ('ProximitySensor' in window) {
-      const proximitySensor = new ProximitySensor();
-  
-      proximitySensor.addEventListener('reading', () => {
-        addToReports(`Proximity: ${proximitySensor.near ? 'Near' : 'Far'}`);
-      });
-  
-      proximitySensor.start();
-    } else {
-      addToReports('Proximity sensor not supported on this device.');
-    }
-  }
-  
-  function startAmbientLightSensor(addToReports) {
-    if ('AmbientLightSensor' in window) {
-      const ambientLightSensor = new AmbientLightSensor();
-  
-      ambientLightSensor.addEventListener('reading', () => {
-        addToReports(`Ambient Light Level: ${ambientLightSensor.illuminance} lux`);
-      });
-  
-      ambientLightSensor.start();
-    } else {
-      addToReports('Ambient light sensor not supported on this device.');
-    }
-  }
-  
-  function startAccelerometer(addToReports) {
-    let accelerometer = new Accelerometer({ frequency: 60 });
-    accelerometer.addEventListener("reading", (e) => {
-      addToReports(`Acceleration along the X-axis: ${accelerometer.x}`);
-      addToReports(`Acceleration along the Y-axis: ${accelerometer.y}`);
-      addToReports(`Acceleration along the Z-axis: ${accelerometer.z}`);
-    });
-    accelerometer.start();
-  }
-  
+
   
   // plot the data we are working with
   async function plot(pointsArray, featureName) {
@@ -184,13 +28,7 @@ async function handleFormSubmit(event) {
         }
     )
   }
-  
-  //denormalisation funtion
-  function denormalise(normalisedTensor, min, max) {
-    const denormalisedTensor = normalisedTensor.mul(max.sub(min)).add(min);
-    return denormalisedTensor;
-  }
-  
+
   //norlisation funtion
   async function normalise(rawTensor) {
     // Ensure the input tensor contains numeric data
@@ -210,7 +48,7 @@ async function handleFormSubmit(event) {
       max: max[0], // Convert tensor data to numeric value
     };
   }
-  
+
   //create model
   function createModel() {
     const model = tf.sequential();
@@ -253,9 +91,6 @@ async function handleFormSubmit(event) {
   async function startModel(selectedSensors) {
     // Load data (needs to be from a URL to work in a browser)
     const strokeDataset = await tf.data.csv("http://localhost:8080/web-server/healthcare-dataset-stroke-data.csv");
-    const sample = strokeDataset.take(10);
-    const dataArray = await sample.toArray();
-    console.log(dataArray);
   
     // Make a map visualization of the data points with tfvis
     const pointsDataset = strokeDataset.map(record => ({
@@ -282,15 +117,11 @@ async function handleFormSubmit(event) {
     // Normalized features and labels
     const normalisedFeature = await normalise(featureTensor);
     const normalisedLabel = await normalise(labelTensor);
-    
-    //denormalise(normalisedFeature.tensor, normalisedFeature.max, normalisedFeature.min).print();
-    //denormalise(normalisedLabel.tensor, normalisedLabel.max, normalisedLabel.min).print();
-  
+
     const [trainingFeatureTensor, testingFeatureTensor] = tf.split(normalisedFeature.tensor, 2);
-    const [trainingLabelTensor, testingLabelTensor] = tf.split(normalisedFeature.tensor, 2);
-  
-    console.log("Training the model with selected sensors:", selectedSensors);
-  
+    const [trainingLabelTensor, testingLabelTensor] = tf.split(normalisedLabel.tensor, 2);
+
+
     // create the model and show its infos
     const model = createModel();
     tfvis.show.modelSummary({name: "model summary"}, model);
